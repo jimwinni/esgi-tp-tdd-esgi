de24cfc8b1735ee6d15abf236fc86ff6b46371fc
jimwinni <jhuynh4@myges.fr>
2022-11-30 16:41:33 +0100

    Refactory des tests et implémentation des services VERTS

--- src/test/java/fr/esgi/cleancode/service/DrivingLicencePenaltyServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicencePenaltyServiceTest.java
@@ -1,15 +1,30 @@
 package fr.esgi.cleancode.service;
 
 import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.exception.ResourceNotFoundException;
 import fr.esgi.cleancode.model.DrivingLicence;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
 import java.util.UUID;
 
+@ExtendWith(MockitoExtension.class)
 public class DrivingLicencePenaltyServiceTest {
 
-    private InMemoryDatabase database = InMemoryDatabase.getInstance();
+    @InjectMocks
+    private DrivingLicencePenaltyService service;
+    @Mock
+    private InMemoryDatabase database;
+    @Captor
+    private ArgumentCaptor<UUID> idCaptor;
+    @Captor
+    private ArgumentCaptor<DrivingLicence> drivingLicenceArgumentCaptor;
 
     @Test
     void should_reduce_points() {
@@ -17,19 +32,37 @@ public class DrivingLicencePenaltyServiceTest {
         final var points = 5;
         final var drivingLicence = DrivingLicence.builder().id(id).build();
 
-        assertThat(drivingLicence.getAvailablePoints() - points)
+        assertThat(points)
                 .isNotNull()
                 .isGreaterThanOrEqualTo(0)
                 .isLessThanOrEqualTo(12);
 
-        Assertions.assertThatNoException().isThrownBy(() -> database.save(id, drivingLicence));
+        Mockito.lenient().when( database.save(id,drivingLicence)).thenReturn(drivingLicence);
+        Mockito.lenient().when( database.findById(id)).thenReturn(Optional.ofNullable(drivingLicence));
 
-        final var updatedDrivingLicence = DrivingLicence.builder().id(id).availablePoints(drivingLicence.getAvailablePoints() - points).build();
+        final var newDrivingLicence = DrivingLicence.builder().id(drivingLicence.getId()).availablePoints(12 - points).build();
 
-        Assertions.assertThatNoException().isThrownBy(() -> database.save(id, updatedDrivingLicence));
+        service.reduceDrivingLicencePoints(id,points);
+
+        Mockito.verify(database).save(idCaptor.capture(), drivingLicenceArgumentCaptor.capture());
+
+        final var updatedDrivingLicenceId = idCaptor.getValue();
+        final var updatedDrivingLicence = drivingLicenceArgumentCaptor.getValue();
+
+        assertThat(updatedDrivingLicenceId)
+                .isEqualTo(newDrivingLicence.getId());
 
         assertThat(updatedDrivingLicence.getAvailablePoints())
-                .isEqualTo(drivingLicence.getAvailablePoints() - points);
+                .isEqualTo(newDrivingLicence.getAvailablePoints());
     }
+    @Test
+    void should_not_reduce_points() {
+        final var id = UUID.randomUUID();
+        final var points = 12;
 
+        ResourceNotFoundException thrown = org.junit.jupiter.api.Assertions.assertThrows(ResourceNotFoundException.class, () -> service.reduceDrivingLicencePoints(id,points),
+                "ResourceNotFoundException error was expected");
+
+        org.junit.jupiter.api.Assertions.assertEquals("Driving Licence doesn't exist in database or Points can't be below 0",thrown.getMessage());
+    }
 }
--- src/test/java/fr/esgi/cleancode/service/DrivingLicenceSaveServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceSaveServiceTest.java
@@ -1,34 +1,57 @@
 package fr.esgi.cleancode.service;
 
 import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.exception.InvalidDriverSocialSecurityNumberException;
 import fr.esgi.cleancode.model.DrivingLicence;
+import lombok.val;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.UUID;
 
+@ExtendWith(MockitoExtension.class)
 public class DrivingLicenceSaveServiceTest {
-    private InMemoryDatabase database = InMemoryDatabase.getInstance();
-    private DrivingLicenceIdGenerationService drivingLicenceIdGenerationService = new DrivingLicenceIdGenerationService();
+    @InjectMocks
+    private DrivingLicenceSaveService service;
+    @Mock
+    private InMemoryDatabase database;
+    @Mock
+    private DrivingLicenceIdGenerationService drivingLicenceIdGenerationService;
 
-    @Test
-    void should_validate() {
-        final var id = drivingLicenceIdGenerationService.generateNewDrivingLicenceId();
-        final var socialSecurityNumber = "123456789123459";
+    @ParameterizedTest
+    @ValueSource(strings = {"199782154684654", "555482154684654"})
+    void should_validate(String validSSNumber) {
 
-        assertThat(socialSecurityNumber)
+        assertThat(validSSNumber)
                 .isNotNull()
                 .containsOnlyDigits()
                 .hasSizeGreaterThanOrEqualTo(15)
                 .hasSizeLessThanOrEqualTo(15);
 
-        final var drivingLicence = DrivingLicence.builder().driverSocialSecurityNumber(socialSecurityNumber).build();
+        val drivingLicence = DrivingLicence.builder().driverSocialSecurityNumber(validSSNumber).build();
 
-        Assertions.assertThatNoException().isThrownBy(() -> database.save(id, drivingLicence));
+        Mockito.lenient().when( service.saveNewDrivingLicence(validSSNumber)).thenReturn(drivingLicence);
 
         assertThat(drivingLicence.getDriverSocialSecurityNumber())
-                .isEqualTo(socialSecurityNumber);
+                .isEqualTo(validSSNumber);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"lorem ipsum", "azertyuiop","09876543210987654321","098654"})
+    void should_not_validate(String invalidSSNumber) {
+
+        InvalidDriverSocialSecurityNumberException thrown = org.junit.jupiter.api.Assertions.assertThrows(InvalidDriverSocialSecurityNumberException.class, () -> service.saveNewDrivingLicence(invalidSSNumber),
+                "InvalidDriverSocialSecurityNumberException error was expected");
+
+        org.junit.jupiter.api.Assertions.assertEquals("Social Security Number Invalid !",thrown.getMessage());
     }
 }
